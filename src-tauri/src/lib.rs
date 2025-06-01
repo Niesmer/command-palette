f4fea17d4228384678038bf80bcc95a64efe2d6a
Antoine <antoine.ferron.etu@univ-lemans.fr>
2025-06-01 12:26:39 +0200

    add prevent close to the window make it so that the window can always be called

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,18 +2,25 @@ use tauri::{AppHandle, Manager};
 
 fn open_or_close_window(app: AppHandle) {
     let window = app.get_webview_window("window");
-        let unwrapped_window = window.unwrap();
-        if unwrapped_window.is_visible().ok().unwrap() {
-            let _ = unwrapped_window.hide();
-        } else {
-            let _ = unwrapped_window.show();
-            let _ = unwrapped_window.set_focus();
-        }
+    let unwrapped_window = window.unwrap();
+    if unwrapped_window.is_visible().ok().unwrap() {
+        let _ = unwrapped_window.hide();
+    } else {
+        let _ = unwrapped_window.show();
+        let _ = unwrapped_window.set_focus();
     }
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .on_window_event(|window, event| match event {
+            tauri::WindowEvent::CloseRequested { api, .. } => {
+                api.prevent_close(); //TODO : have a way to really quit the app via system tray
+                let _ = window.hide();
+            }
+            _ => {}
+        })
         .setup(|app| {
             #[cfg(desktop)]
             {
