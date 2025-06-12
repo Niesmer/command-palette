f69a5b0a255b113d838d7af8e8f71965f670d9f3
Niesmer <antoine.ferron.pro@pm.me>
2025-06-12 13:04:34 +0200

    remove useless ref

--- src/components/CommandList.tsx
+++ src/components/CommandList.tsx
@@ -1,4 +1,3 @@
-import { useRef, useState } from 'react';
 import { generate_commands_from_string } from '../assets/commands';
 import Command from './Command';
 
@@ -7,11 +6,8 @@ type CommandListProps = {
 }
 
 const CommandList = ({ search }: CommandListProps) => {
-    const listRef = useRef<HTMLElement>(null);
-
-
     return (
-        <div ref={listRef}>
+        <div>
             <ul>
                 {generate_commands_from_string(search).map((command, index) =>
                     <li key={index} className='focus-within:bg-red-200 has-active:bg-green-200 px-2 py-1'>
